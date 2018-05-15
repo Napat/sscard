d92db26d9ac46ae125ebf1250770d7f4de4756a4
Atomgun <likhit.lkns@gmail.com>
2018-05-15 21:19:43 +0700

    SIM : APDU Class 00 test

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Super Smart Card API on top of pcsc.
 # Linux: install pcsc library
 sudo apt-get install pcscd
 
+# run pcscd 
+sudo LIBCCID_ifdLogLevel=0x000F pcscd --foreground --apdu --color | tee log.txt
+
 # goget
 go get -u github.com/Napat/sscard/sscard
 
@@ -15,12 +18,13 @@ go get -u github.com/Napat/sscard/sscard
 go build -o sscard github.com/Napat/sscard/main
 
 ./sscard  # ./sscard.exe on windows
-```
-
-About requirement and other platform see: `docs/INSTALLATION_xxx.md`
 
 # go run hack
 go run $(find ./ | grep ./main/)
+
+```
+
+About requirement and other platform see: `docs/INSTALLATION_xxx.md`
 
 ## Platforms
 
@@ -31,3 +35,4 @@ go run $(find ./ | grep ./main/)
 
 - [PCSC in golang](https://ludovicrousseau.blogspot.fr/2016/09/pcsc-sample-in-go.html)
 - [APDU command for Thai ID card](https://github.com/Napat/ThaiNationalIDCard/blob/master/APDU.md)
+- [APDU RESP List](https://www.eftlab.co.uk/index.php/site-map/knowledge-base/118-apdu-response-list)
--- main/example_simcard.go
+++ main/example_simcard.go
@@ -48,19 +48,42 @@ func exampleSimCard() {
 		fmt.Println("Error Transmit:", err)
 		return
 	}
-	fmt.Printf("resp sscard.APDUReadICCID: % 02x\r\n", iccid)
+	fmt.Printf("resp APDUReadICCID: % 02x\r\n", iccid)
 	iccidDecode := sscard.DecodeICCID(iccid)
-	fmt.Printf("ICCID Decode : % 02x Valid : %v\r\n\n", iccidDecode, sscard.LuhnByteValid(iccidDecode[:10], 20))
+	// fmt.Printf("ICCID Decode : % 02x Valid : %v\r\n\n", iccidDecode, sscard.LuhnByteValid(iccidDecode[:10], 20))
+	fmt.Printf("ICCID Decode : % 02x\r\n\n", iccidDecode)
+
+	// APDUReadICCIDclass00
+	iccid00, err := sscard.APDUGetRsp(card, sscard.APDUReadICCIDclass00)
+	if err != nil {
+		fmt.Println("Error Transmit:", err)
+		return
+	}
+	fmt.Printf("resp APDUReadICCIDclass00: % 02x\r\n", iccid00)
+	iccid00Decode := sscard.DecodeICCID(iccid00)
+	// fmt.Printf("ICCID Decode : % 02x Valid : %v\r\n\n", iccidDecode, sscard.LuhnByteValid(iccidDecode[:10], 20))
+	fmt.Printf("ICCIDclass00 Decode : % 02x\r\n\n", iccid00Decode)
 
 	imsi, err := sscard.APDUGetRsp(card, sscard.APDUReadIMSI)
 	if err != nil {
 		fmt.Println("Error Transmit:", err)
 		return
 	}
-	fmt.Printf("resp sscard.APDUReadIMSI: % 02x\r\n", imsi)
+	fmt.Printf("resp APDUReadIMSI: % 02x\r\n", imsi)
 	imsiDecode := sscard.DecodeIMSI(imsi)
-	fmt.Printf("IMSI Decode : % 02x Valid : %v\r\n\n", imsiDecode, sscard.LuhnByteValid(imsiDecode[:9], 18))
+	// fmt.Printf("IMSI Decode : % 02x Valid : %v\r\n\n", imsiDecode, sscard.LuhnByteValid(imsiDecode[:9], 18))
+	fmt.Printf("IMSI Decode : % 02x\r\n\n", imsiDecode)
 
+	imsi00, err := sscard.APDUGetRsp(card, sscard.APDUReadIMSIclass00)
+	if err != nil {
+		fmt.Println("Error Transmit:", err)
+		return
+	}
+	fmt.Printf("resp APDUReadIMSIclass00: % 02x\r\n", imsi00)
+	imsi00Decode := sscard.DecodeIMSI(imsi00)
+	// fmt.Printf("IMSI Decode : % 02x Valid : %v\r\n\n", imsiDecode, sscard.LuhnByteValid(imsiDecode[:9], 18))
+	fmt.Printf("IMSIclass00 Decode : % 02x\r\n\n", imsi00Decode)
+	return
 	// APDUReadSMS
 	sms, err := sscard.APDUGetRsp(card, sscard.APDUReadSMS)
 	if err != nil {
